ab62d9bb8f3f23e847078d0bce720f844fbf7824
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-11-21 18:06:44 -0300

    Implementando teste com login dinâmico
    Foi implementado testes com login dinâmico, pois, a página sempre que recarrega traz um campo diferente ou dia, ou mês, ou ano. Encontrei o formulário que tinha o id= login guardei dentro de uma variável e usei um método do ruby case e depois usei o when. Usei uma técnica de conteins se tem o dia o teste faz uma coisa, se tem mês faz outra coisa, se tem ano faz outra coisa.

---
+++ spec/login2_spec.rb
@@ -0,0 +1,28 @@
+
+describe 'Forms', :login2 do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/login2'
+    end
+
+    it 'login com data de nascimento' do
+        find('#userId').set 'stark'
+        find('#passId').set 'jarvis!'
+        # A página é dinâmica sempre que recarrego traz um campo diferente ou dia, ou mês, ou ano. 
+
+        login_form = find('#login')
+
+        case login_form.text
+        when /Dia/
+            find('#day').set '29'
+        when /Mês/
+            find('#month').set '05'
+        when /Ano/
+            find('#year').set '1970'
+        end
+        sleep 2
+
+        click_button 'Login'
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+    end
+end
