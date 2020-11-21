7158d6dca33219323831aad55c739a2b76ef3c96
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-11-21 20:27:31 -0300

    Adicionando Arquivos de configuração
    No arquivo spec_helper no bloco de configurações do capybara, configuramos para que o computador não precisa subir o Chrome, eles serão rodados, mas o Chrome não irá abrir.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,9 @@ end
 
 # Na linha a baixo eu informo que eu vou usar o selenium no google chrome
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  config.default_driver = :selenium_chrome_headless
+  # Caso eu queira que os testes rodem sem eu subir o Chrome e ficar vendo a página basta eu passar :selenium_chrome_headless
+  # Isso pode ser útil pois eu posso rodar dentro de um servidor que não tem interface visual
   # Caso queira automatizar com o firefox basta trocar o selenium_chrome por apenas selenium
-  config.default_max_wait_time = 5
+  config.default_max_wait_time = 15
 end
