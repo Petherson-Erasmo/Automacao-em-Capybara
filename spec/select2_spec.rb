1b6ab956514ba3cb9dd334d492e4dff7555e4a8b
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-11-21 15:28:20 -0300

    Implementando testes com select2
    Nesse commit foi implementado o teste com select2 multiplo, nele podemos selecionar mais de uma opção. Criei um método para não precisar ficar repetindo código já que iria selecionar mais de um ator. Eu poderia usar o     selecione('Nome_do_ator')     porém, eu iria ter que chamar o método toda vez que fosse querer selecionar outro autor. Para melhorar ainda mais os testes criamos um array, com os atores que queríamos selecionar.

--- spec/select2_spec.rb
+++ spec/select2_spec.rb
@@ -20,9 +20,28 @@ describe 'Select 2', :select2 do
         end
     end
 
-    describe('Multiple') do
+    describe('Multiple', :multi) do
         before(:each) do
             visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/multi.html'
+        end
+
+        # eu criei um método para não precisar ficar repetindo código
+        def selecione(ator)
+            find('.select2-selection--multiple').click
+            find('.select2-search__field').set ator
+            find('.select2-results__option').click
+        end
+
+        it 'Seleciona atores' do
+            # Da forma a seguiar vamos selecionar uma ator por vez passando o metodo selecione que criamos
+            # selecione('Jim Carrey')
+            # selecione('Owen Wilson')
+
+            # Para melhorar ainda mais os testes criamos um array
+            atores = ['Jim Carrey','Owen Wilson','Kevin James']
+            atores.each do |um_ator|
+                selecione(um_ator)
+            end
         end
     end
 end
