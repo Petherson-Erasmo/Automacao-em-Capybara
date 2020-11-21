d52576c99312f95b486d5c443b4b9ee343a293d8
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-11-21 00:48:59 -0300

    Implementando teste com Drag and Drop
    Foi feito um teste para mover um elemento de uma div para outra (drag and drop) e fizemos uma dupla validação.

---
+++ spec/drag_drop_spec.rb
@@ -0,0 +1,21 @@
+
+
+describe 'Caixa de seleção', :drag do
+    # o , :drag significa uma tag
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/drag_and_drop'
+    end
+
+    it 'Homem Aranhapertence ao time do Stark' do
+        # o .team-stark é o elemento pai e o .column é o elemento filho
+        stark = find('.team-stark .column')
+        cap = find('.team-cap .column')
+
+        spiderman = find('img[alt$=Aranha]')
+        spiderman.drag_to stark
+
+        expect(stark).to have_css 'img[alt$=Aranha]'
+        expect(cap).not_to have_css 'img[alt$=Aranha]'
+    end
+end
