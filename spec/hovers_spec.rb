2274efd08e0f6b60f722268fd5b4f5a3f80633d9
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-11-21 00:32:54 -0300

    Adcionando expressão regular
    Foi add algumas expressões regulares no arquivo de teste para melhorar a busca, do elemento.

--- spec/hovers_spec.rb
+++ spec/hovers_spec.rb
@@ -14,16 +14,22 @@ describe 'Caixa de seleção', :hover do
     end
 
     it 'Quando passo o mause sobre o Pantera negra' do
-        card = find('img[alt="Pantera Negra"]')
+        # o ^ serve para indicar a primeira palavra. 
+        # No caso vai trazar uma imagem que tem a tag alt que o contúdo começa com Pantera
+        card = find('img[alt^=Pantera]')
         card.hover
 
         expect(page).to have_content 'Nome: Pantera Negra'
     end
         
         it 'Quando passo o mause sobre o Homem Aranha' do
-            card = find('img[alt="Homem Aranha"]')
+            # o $ serve para indicar a ultima palavra. 
+            # No caso vai trazar uma imagem que tem a tag alt que o contúdo termina com Aranha
+            card = find('img[alt$=Aranha]')
             card.hover
     
             expect(page).to have_content 'Nome: Homem Aranha'
     end
+    # tem também o * que serve para indicar que a tag contem. 
+    # No caso vai trazer uma imagem que tem a tag alt que contem a palavra aranha
 end
