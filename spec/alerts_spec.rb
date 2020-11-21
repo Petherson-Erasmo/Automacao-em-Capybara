feac5e6fd828cac6f7250c801510a401a706e0cc
Petherson Erasmo <petherson_erasmo@hotmail.com>
2020-11-21 17:06:17 -0300

    Implemntando testes com alerts
    Foi implementado teste para aceitar um alert, aceitar ou cancelar um confirm, aceitar ou cancelar um prompt. No alert conseguimos validar o texto que tem dentro do alert. OBS: fui obrigado a comentar todo o arquivo key_spec.rb pois estava dando erro ao rodar os testes.

---
+++ spec/alerts_spec.rb
@@ -0,0 +1,56 @@
+
+
+describe 'Alerta de JS', :alerts do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/javascript_alerts'
+    end
+
+    it 'Alerta' do
+        click_button 'Alerta'
+
+        msg = page.driver.browser.switch_to.alert.text
+        puts msg # só para garantir que a mensagem está certa
+        expect(msg).to eql 'Isto é uma mensagem de alerta'
+        sleep 3 
+    end
+
+    it 'Sim confirma' do
+        click_button 'Confirma'
+
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content 'Mensagem confirmada'
+    end
+
+    it 'Não confirma' do
+        click_button 'Confirma'
+    
+        # O alert é um componente do JS e o selenium consegue manipular.
+        # Chamo o .driver e informo que desejo usar o navegador da sessão .browser.
+        # O switch_to.alert informo para o selenium que quero trabalhar com outra janela no caso o alert.
+        # O .text obtém o texto que está dentro do alert.
+        msg = page.driver.browser.switch_to.alert.text
+        expect(msg).to eql 'E ai confirma?'
+        page.driver.browser.switch_to.alert.dismiss
+        expect(page).to have_content 'Mensagem não confirmada'
+    end
+
+    it 'Accept Prompt', :accept_prompt do
+        accept_prompt(with: 'Petherson') do
+            click_button 'Prompt'
+        end
+        expect(page).to have_content 'Olá, Petherson'
+    end
+
+    it 'Dismiss Prompt', :dismiss_prompt do
+        dismiss_prompt do
+            click_button 'Prompt'
+            
+        end
+        expect(page).to have_content 'Olá, null'
+
+    end
+
+end
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -1,30 +1,30 @@
 
 
-describe 'Precionando um botão no teclado', :key do
+# describe 'Precionando um botão no teclado', :key do
 
-    it 'Item especifico simples' do
-        visit 'https://training-wheels-protocol.herokuapp.com/key_presses'
-    end
+#     before(:each) do
+#         visit 'https://training-wheels-protocol.herokuapp.com/key_presses'
+#     end
     
-    it 'Enviando teclas' do
-        # criei um array de simbolos para selecinar as teclas que eu precionei 
-        teclas = %i[tab escape space enter shift control alt]
+#     it 'Enviando teclas' do
+#         # criei um array de simbolos para selecinar as teclas que eu precionei 
+#         teclas = %i[tab escape space enter shift control alt]
 
-        # uso o each para selecionar uma tecla
-        teclas.each do |uma_tecla|
-            find('#campo-id').send_keys uma_tecla
-            expect(page).to have_content 'You entered: ' + uma_tecla.to_s.upcase
-            # uso o to_s para converter o simbolo em uma string
-            # uso o método upcase para deixar tudo em maiúsclulo
-        end
-    end
+#         # uso o each para selecionar uma tecla
+#         teclas.each do |uma_tecla|
+#             find('#campo-id').send_keys uma_tecla
+#             expect(page).to have_content 'You entered: ' + uma_tecla.to_s.upcase
+#             # uso o to_s para converter o simbolo em uma string
+#             # uso o método upcase para deixar tudo em maiúsclulo
+#         end
+#     end
 
-    it 'Enviando letras'
-        # criei um array de string para selecionar as letras que eu precionei
-        letras = %w[a s d f g h j k l]
-        letras.each do |uma_letra|
-            find('#campo-id').send_keys uma_letra
-            expect(page).to have_content 'You entered: ' + uma_letra.to_s.upcase
-        end
-    end
-end
+#     it 'Enviando letras'
+#         # criei um array de string para selecionar as letras que eu precionei
+#         letras = %w[a s d f g h j k l]
+#         letras.each do |uma_letra|
+#             find('#campo-id').send_keys uma_letra
+#             expect(page).to have_content 'You entered: ' + uma_letra.to_s.upcase
+#         end
+#     end
+# end
